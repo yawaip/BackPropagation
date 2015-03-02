a330ddef7c1cbabe53a589a1b40d71d4caf710c2
yawaip <Sawai@SawaiD>
2015-03-03 01:32:39 +0900

    確認用のコードを追加2

--- src/MatrixStyle.scala
+++ src/MatrixStyle.scala
@@ -15,7 +15,7 @@ object MatrixStyle {
     val output_num = 1;
     val eta = 0.1;
     
-    val N = 5;
+    val N = 100;
     val temp_input = ( -1.0 to 1.0 by 2.0 / N ).toArray;
     val temp_inst = temp_input.map { v => 0.5 * ( Math.sin ( Math.PI * v ) + 1)};
     val input = ((arr:Array[Double]) => {
@@ -41,7 +41,7 @@ object MatrixStyle {
  		var w1 = DenseMatrix.zeros[Double]( hidden_num, signal_num).map{v => rand.nextDouble()*2 - 1};
     var w2 = DenseMatrix.zeros[Double]( output_num, hidden_num).map{v => rand.nextDouble()*2 - 1};
 
-    for ( loop <- 1 to 1){
+    for ( loop <- 1 to 10000){
     	for( input_num <- 0 until input.length ){
         
         val in = input(input_num);
@@ -53,29 +53,16 @@ object MatrixStyle {
         //誤差を計算
         val inst = DenseVector(instraction(input_num)); //教師信号
         val error = (x3 - inst).map{ v => 0.5 * v * v }.sum; 
-        //print(inst);print(" ");print(x3);print(" ");println(error);
         //出力層の誤差を計算
         val error_out = (x3-inst) * x3.t * (1.0-x3);
-        /*
-        println("error_out "+error_out);
-        println("x2 " + x2);
-        println("w2 " + w2);
-        */
+        
         //隠れ層の誤差を計算
-        //val error_hidden = ((w2.t * error_out) * x2.t) * ( 1.0 - x2);
-        //println("***");
         val error_hidden = diag(diag(x2*(w2.t*error_out).t) * (1.0 - x2).t);
-        //println(error_hidden);
-        //println("+++")
-        //val error_hidden = ( w2.t * error_out  ) * x2.t //* ( 1.0 - x2 );
-        //val error_hidden = ((w2.t * error_out ) * x2.t) * ( 1.0 - x2);// これか？
-        //val error_hidden = (w2*error_out)*x2.t*(1.0-x2);
-       // val error_hidden = x2.t * (w2 * error_out); 
         
         //出力層 の誤差を計算
         var epsi_out = new Array[Double](x3.length);
         for(i <- 0 until epsi_out.length){
-          epsi_out(i) = (x3(i) - inst(i)) * x3(i) * ( 1.0 - x3(i) );
+          epsi_out(i) = (x3(i) - inst(i)) * x3(i) * ( 1 - x3(i) );
         }
         
         //隠れ層 の誤差を計算
@@ -86,25 +73,9 @@ object MatrixStyle {
             temp += w2(j,i) * epsi_out(j);
           }
           epsi_hidden(i) = temp * x2(i) *( 1 - x2(i));
-          //println(epsi_hidden(i));
         }
-        println("error_hidden "+error_hidden);
-        println(epsi_hidden.deep);
         
-        //重みの修正
-        /*
-        println(x1.toString() + " " + error_hidden.toString() );
-        println(x1 * error_hidden.t);
-        println(w1);
-        println(x2.toString() + " " + error_out.toString());
-        println(x2 * error_out.t);
-        println(w2);
-        */
-        /*
-        w1 -=  ( x1 * error_hidden.t).t :* eta;
-        w2 -= ( x2 * error_out.t).t :* eta;
-        */
-        //重みの修正 入力→隠れ層
+           //重みの修正 入力→隠れ層
         for(i <- 0 until x1.length){
           for( j <- 0 until x2.length ){
             w1(j,i) -= eta * ( x1(i) * epsi_hidden(j) );
@@ -118,7 +89,6 @@ object MatrixStyle {
           }
         }
     	} 
-      //println (err);
     }
     
     //ここから出力
